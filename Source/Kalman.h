21493c39af8aa1ca7a47ef9023e905600d2610cb
Jake Zeisel <jacobzeisel@gmail.com>
2023-11-11 12:35:05 -0500

    convert_to_hz

--- Source/Kalman.h
+++ Source/Kalman.h
@@ -25,7 +25,7 @@ public:
         this->p = (1.0 - (pow(filter->getPastSampleOne(), 2) / (pow(filter->getPastSampleOne(), 2) + (this->r_value / p_update)))) * p_update;
         if (abs(new_a) < 1.98 || abs(new_a) > 2.0) new_a = getRandom();
         filter->updateFrequency(new_a);
-        return (sampleRate / (2.0*M_PI))*std::acos(new_a / 2.0) + offSet;
+        return convert_to_hz(new_a, sampleRate) + offSet;
     }
     
     void reset(Biquad* filter) {
@@ -33,8 +33,13 @@ public:
         filter->reset();
     }
     
+    float convert_to_hz(float a, float sampleRate) {
+        return (sampleRate / (2.0*M_PI))*std::acos(a / 2.0);
+    }
+    
     void set_q_value(float new_value) {this->q_value = new_value;};
     void set_r_value(float new_value) {this->r_value = new_value;};
+    
 private:
     Biquad* filter;
     float q_value = 0.00005;
